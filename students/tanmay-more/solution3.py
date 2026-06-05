b1c12d9805a74a88f5b6481a04baf8b15710a309
tanmaymore1705-dotcom <tanmaymore1705@gmail.com>
2026-06-05 11:36:15 +0530

    Add consonant counting logic to solution3.py
    Count consonants in a set of letters, excluding vowels.

--- students/tanmay-more/solution3.py
+++ students/tanmay-more/solution3.py
@@ -15,3 +15,21 @@ for x in s:
     else :
         continue 
 print (count)
+
+#pratical no 9
+s={"a", "b", "c", "d", "e", "f", "g", "h","i","j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
+count=0
+for x in s:
+    if x=="a":
+        continue
+    elif x=="e": 
+        continue
+    elif x=="i":
+        continue
+    elif x=="o":
+        continue
+    elif x=="u":
+        continue
+    else :
+        count=count+1
+print (count)
